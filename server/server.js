684c931bc2a78b21428d39bd5429eb9aabadb3b9
Samuel McHugh <smchugh230395@gmail.com>
2018-07-24 11:47:30 -0500

    added createmessage and post message listeners and emitters

--- server/server.js
+++ server/server.js
@@ -14,8 +14,22 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('new user connected')
 
+  // Server is Listening to Client
+  // Server <--data-- Client
   socket.on('disconnect', () => {
     console.log('user was disconnected')
+  })
+
+  socket.on('newMessageCreated', (message) => {
+    console.log('Message created', message)
+  })
+
+  // Server is Shouting to Client
+  // Server --data--> Client
+  socket.emit('newMessagePosted', {
+    user: 'Alice',
+    text: 'Hi everyone',
+    timeStamp: 123
   })
 })
 
